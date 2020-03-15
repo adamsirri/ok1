f56e73c3cf11f3fed45fa5b869957effcd559a77
adamsirri <62217665+adamsirri@users.noreply.github.com>
2020-03-15 22:15:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-mpor discord
+import discord
 from discord.ext import commands
 client = commands.Bot(command_prefix ="/")
 
@@ -15,4 +15,4 @@ async def on_message(message):
 		embed=discord.Embed(title="Confession .",color=0x00ff00, description="A new confession  .")
 		embed.add_field(name="Confession :", value=Nmessage, inline=False)
 		await channel.send(embed=embed)
-client.run("Njg3Mzk1NTU4NDc3NzkxMjQ1.Xm6Ykg.y7nfocWMyt3hnCPEDSS6CAqH5yY",bot=True)
+client.run("Njg3Mzk1NTU4NDc3NzkxMjQ1.Xm6a7Q.JELzhOiC18Exfu3uz4W6ajT4_FQ",bot=True)
